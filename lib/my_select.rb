6432ba1a5972d6ef6d6ecc9d1607d5abd94c547d
alexanderchao123 <chao.alexander.123@gmail.com>
2018-09-08 04:15:24 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
 def my_select(collection)
- # your code here!
+  return "This block should not run!" if collection.empty?
+
+  new_arr = []
+  i = 0
+  while i < collection.length
+    if yield(collection[i])
+      new_arr << collection[i]
+    end
+    i += 1
+  end
+
+  return new_arr
 end
